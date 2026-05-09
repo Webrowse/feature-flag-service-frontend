83a225565500df8974c7f3a538afbf6a960fce16
Webrowse <great.adarsh@gmail.com>
2026-05-10 01:53:53 +0530

    feat: Change the Background

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
-import bgIdea from '@/assets/bg_idea_1.png';
+import bgIdea from '@/assets/bg_idea_2.png';
 
 const features = [
   {
